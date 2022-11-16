b0b20905f571624769fce8574a824caaaab5c9fa
Jeysson Guevara <jeysson_guevara@epam.com>
2022-11-16 15:37:46 -0500

    jcs-36: Created testing helper library.

---
+++ JeyDotC.JustCs.Testing/BaseTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using JeyDotC.JustCs.Html.Attributes;
+
+namespace JeyDotC.JustCs.Testing;
+
+public abstract class BaseTest : ComponentElement
+{
+    [ExcludeFromCodeCoverage]
+    protected override sealed Element Render(IElementAttributes? attributes) => throw new NotImplementedException();
+}
+
